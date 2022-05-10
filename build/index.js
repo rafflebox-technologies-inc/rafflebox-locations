8b314d9f1669d20caef47060f922ba8ca94f8bc0
Basharfrancis <bashar.francis@rafflebox.ca>
2022-05-10 10:06:27 -0300

    update version

--- build/index.js
+++ build/index.js
@@ -1,13 +1,11 @@
-'use strict';
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
-Object.defineProperty(exports, '__esModule', { value: true });
-exports.validateUsPostalCodes = exports.validateCaPostalCodes = void 0;
-const ca_postal_codes_1 = __importDefault(require('./ca-postal-codes'));
-exports.validateCaPostalCodes = ca_postal_codes_1.default;
-const us_zip_codes_1 = __importDefault(require('./us-zip-codes'));
-exports.validateUsPostalCodes = us_zip_codes_1.default;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUEsd0VBQXNEO0FBRzdDLGdDQUhGLHlCQUFxQixDQUdFO0FBRjlCLGtFQUFtRDtBQUVuQixnQ0FGekIsc0JBQXFCLENBRXlCIn0=
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.isValidateUsPostalCodes = exports.isValidateCaPostalCodes = void 0;
+const ca_postal_codes_1 = __importDefault(require("./ca-postal-codes"));
+exports.isValidateCaPostalCodes = ca_postal_codes_1.default;
+const us_zip_codes_1 = __importDefault(require("./us-zip-codes"));
+exports.isValidateUsPostalCodes = us_zip_codes_1.default;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUEsd0VBQXdEO0FBRy9DLGtDQUhGLHlCQUF1QixDQUdFO0FBRmhDLGtFQUFxRDtBQUVuQixrQ0FGM0Isc0JBQXVCLENBRTJCIn0=
